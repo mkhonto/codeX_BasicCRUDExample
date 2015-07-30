c6911cf2ce6f018920d77d6e295a184efac3ead6
mkhonto <jabulani@codex.co>
2015-07-30 10:48:21 +0200

    done creating script for sales table from the products table and joining sales_csv with products using product name column

--- nelisa_script.sql
+++ nelisa_script.sql
@@ -1,3 +1,9 @@
+drop table if exists Purchases;
+drop table if exists Sales;
+drop table if exists Suppliers;
+drop table if exists Products;
+drop table if exists Categories;
+
 create table Categories (
 	 id int not null primary key auto_increment, 
 	name varchar(35) 
@@ -18,6 +24,7 @@ create table Sales (
 	id int not null primary key auto_increment,
 	date char(35), 
     products_id int,
+    product_name varchar(35),
 	no_sold int,
 	selling_Price int, 
     foreign key(Products_id) references Products(id)
